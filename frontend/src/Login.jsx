ed1bc76a42f453f531cc804c3e994ecb9dc81f04
Neel Patel <patelneel0445@gmail.com>
2025-06-27 21:32:13 +0530

    Refactor Login component layout and update CSS styles for improved design

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,35 +3,37 @@ import styles from "./Login.module.css";
 
 const Login = () => {
   return (
-    <div className={styles["login-container"]}>
-      <div className={styles["login-box"]}>
-        <h2 className={styles.h2}>Login</h2>
-        <form>
-          <div className={styles["input-group"]}>
-            <label htmlFor="email">Username</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter your Username"
-              required
-            />
-          </div>
-          <div className={styles["input-group"]}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
-          <button className={styles.button} type="submit">
-            Login
-          </button>
-          <p className={styles["signup-link"]}>
-            Don't have an account? <a href="/sign-up">Sign up</a>
-          </p>
-        </form>
+    <div className="h-screen bg-blue-300 flex justify-center items-center">
+      <div className={styles["login-container"]}>
+        <div className={styles["login-box"]}>
+          <h2 className={styles.h2}>Login</h2>
+          <form>
+            <div className={styles["input-group"]}>
+              <label htmlFor="email">Username</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your Username"
+                required
+              />
+            </div>
+            <div className={styles["input-group"]}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+            <button className={styles.button} type="submit">
+              Login
+            </button>
+            <p className={styles["signup-link"]}>
+              Don't have an account? <a href="/sign-up">Sign up</a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
